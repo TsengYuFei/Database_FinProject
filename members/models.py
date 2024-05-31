cadca91fe3f46c5b3298d4c694824b5b83e2d333
YinX518 <930518ivy2004@gmail.com>
2024-05-31 16:44:28 +0800

    記得ul4

--- members/models.py
+++ members/models.py
@@ -6,6 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from viewflow.fields import CompositeKey
 
 
 class Administrator(models.Model):
@@ -99,6 +100,7 @@ class Book(models.Model):
     class Meta:
         managed = False
         db_table = 'book'
+
 
 
 class BookAuthor(models.Model):
